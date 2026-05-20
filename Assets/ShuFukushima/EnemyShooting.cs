4ab4ca9d39e1ab74e5ecfc4c87d3b9233d165d1c
Shu_Fukushima <keiranhiragi@gmail.com>
2026-05-20 13:49:32 +0900

    アタックバフの作成

--- Assets/ShuFukushima/EnemyShooting.cs
+++ Assets/ShuFukushima/EnemyShooting.cs
@@ -5,6 +5,7 @@ public class EnemyShooting : MonoBehaviour
     public float ShootingInterval;
     public float ShootingTime = 0f;
     public float BulletSpeed;
+    public float MissileSpeed;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform muzzle;
     [SerializeField] private Transform playerTransform;
@@ -22,23 +23,29 @@ public class EnemyShooting : MonoBehaviour
 
         if (ShootingTime >= ShootingInterval)
         {
-            Vector3 playerDir = (playerTransform.position - transform.position).normalized;
-
-            float angle = Mathf.Atan2(playerDir.y, playerDir.x) * Mathf.Rad2Deg;
-
-            // 生成位置を取得
-            Vector3 playerPos = transform.position;
-            // 弾を生成
-            GameObject bullet = Instantiate(bulletPrefab, muzzle.position, Quaternion.Euler(0, 0, angle - 90f));
-
-            if (bullet.TryGetComponent(out Rigidbody2D rb2D))
-            {
-                Vector2 velocity = playerDir * BulletSpeed; // * Time.deltaTime;
-                rb2D.linearVelocity = velocity;
-            }
-
-            ShootingTime = 0f;
+            NormalBullet();
         }
 
     }
+
+    void NormalBullet()
+    {
+        Vector3 playerDir = (playerTransform.position - transform.position).normalized;
+
+        float angle = Mathf.Atan2(playerDir.y, playerDir.x) * Mathf.Rad2Deg;
+
+        // 生成位置を取得
+        Vector3 playerPos = transform.position;
+        // 弾を生成
+        GameObject bullet = Instantiate(bulletPrefab, muzzle.position, Quaternion.Euler(0, 0, angle - 90f));
+
+        if (bullet.TryGetComponent(out Rigidbody2D rb2D))
+        {
+            Vector2 velocity = playerDir * BulletSpeed; // * Time.deltaTime;
+            rb2D.linearVelocity = velocity;
+        }
+
+        ShootingTime = 0f;
+    }
+
 }
