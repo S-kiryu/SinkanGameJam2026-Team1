056eb3061725c16de0c424e9de66049c08c10c9b
KINOKO\nubob <Ki28ryu@icloud.com>
2026-05-20 16:18:47 +0900

    [add]ボスのステータスを作成

---
+++ Assets/Sugimoto/Script/Boss/BossStatus.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class BossStatus : MonoBehaviour
+{
+    [SerializeField] private float _maxHp = 100f;
+    private float _currentHp;
+
+    public float MaxHp => _maxHp;
+    public float CurrentHp => _currentHp;
+
+    public System.Action<float, float> OnHpChanged;
+
+    private void Awake()
+    {
+        _currentHp = _maxHp;
+        OnHpChanged?.Invoke(MaxHp, CurrentHp);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _currentHp -= damage;
+
+        OnHpChanged?.Invoke(MaxHp, CurrentHp);
+
+        if (_currentHp <= 0)
+        {
+            _currentHp = 0;
+            BossDead();
+        }
+    }
+
+    public void BossDead()
+    {
+        Debug.Log("Boss defeated!");
+    }
+}
