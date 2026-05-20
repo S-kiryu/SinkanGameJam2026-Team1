5fc1beb16f65ea3f48801e0e7a8c0b7cd7f11fea
KINOKO\nubob <Ki28ryu@icloud.com>
2026-05-20 17:53:25 +0900

    [add]敵の攻撃

---
+++ Assets/Sugimoto/Script/Boss/BulletAttack.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EnemyAttack/Bullet")]
+public class BulletAttack : EnemyAttackBase
+{
+    public GameObject bulletPrefab;
+    public float speed;
+
+    protected override void Execute(Transform enemy, Transform muzzle, Transform player)
+    {
+        Vector3 dir = (player.position - enemy.position).normalized;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        GameObject bullet = Instantiate(bulletPrefab, muzzle.position, Quaternion.Euler(0, 0, angle - 90f));
+
+        if (bullet.TryGetComponent(out Rigidbody2D rb))
+        {
+            rb.linearVelocity = dir * speed;
+        }
+    }
+}
