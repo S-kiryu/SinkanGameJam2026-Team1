679772bc5776e960da9bd8c8bf17033d991ae2cf
kzuya1105 <kzuya1105@outlook.jp>
2026-05-21 12:53:42 +0900

    ローテーション

--- Assets/MurataKazuya/KaitenTossin.cs
+++ Assets/MurataKazuya/KaitenTossin.cs
@@ -1,33 +1,70 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class KaitenTossin : MonoBehaviour
-{   bool Switch = false;
+{
+    
+    bool Switch = false;
     [SerializeField] private Transform startBoss;
-    [SerializeField] private Transform player;
-    [SerializeField] private float moveSpeed = 5f;
+    
+    private float minDistance = 2f;
+
+    [SerializeField] private float _moveSpeed = 5f;
     [SerializeField] public float _rotateSpeed = 1f;
+    [SerializeField] private Transform Player;
 
     void Update()
     {
+        CheckDistance();
+
+        float distance = Vector3.Distance(transform.position, Player.position);
         transform.Rotate(0, 0, _rotateSpeed * Time.deltaTime);
-        if (player != null && Switch == false)
+
+        Debug.Log(distance);
+
+        if (Player != null && Switch == false)
         {
-            Vector3 direction = (player.position - transform.position);
-            transform.position += direction.normalized * moveSpeed * Time.deltaTime;
+            Vector3 direction = (Player.position - transform.position);
+            transform.position += direction.normalized * _moveSpeed * Time.deltaTime;
+
+        }
+        else if(Player != null && Switch == true)
+        {
 
         }
     }
-    private void OnTriggerEnter2D(Collider2D other)
+
+
+
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             {
                 //Vector3 enddirection = (startBoss.position - transform.position);
                 //transform.position += enddirection.normalized * moveSpeed * Time.deltaTime;
-                //Switch = true;
-                
+
+                //transform.position = Vector3.MoveTowards(transform.position, startBoss.position, _moveSpeed * Time.deltaTime);
+                Switch = true;
             }
+        }
+
+    }
+
+    void CheckDistance()
+    {
+        GameObject playerT = GameObject.FindGameObjectWithTag("Player");
+
+        float distance = Vector2.Distance(transform.position, playerT.transform.position);
+
+        if(distance <= minDistance)
+        {
+            Switch = true;
+        }
+        else
+        {
+            Switch = false;
         }
 
     }
